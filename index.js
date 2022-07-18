466a5007ff3164c4d46021c750f429afd1783529
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-18 13:44:09 -0500

    Remove input element, change css, fix alignment

---
+++ index.js
@@ -0,0 +1,25 @@
+//add
+function add(a,b){
+    return a + b;
+}
+//subtract
+function subtract(a,b){
+    return a - b;
+}
+//multiply
+function multiply(a,b){
+    return a * b;
+}
+//divide
+function divide(a, b){
+    if(b === 0){
+        alert("You can't divide by zero!");
+    } else{
+        return a / b;
+    }
+}
+//evaluate
+function evaluate(str){
+    //parse the string and store in an arr
+    //check the symbol and call the correct function
+}
