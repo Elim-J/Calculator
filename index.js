61f2d130918d4f546ca25bba29b2eab786e57547
elim-j <01CodePathways11@gmail.com>
2022-07-18 15:07:50 -0500

    Add pseudocode for evaluate, addNumber, addOperator

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ function divide(a, b){
 }
 //evaluate
 function evaluate(str){
-    //parse the string and store in an arr
-    //check the symbol and call the correct function
+    //when equal sign is pressed
+    //if regex expression contains number symbol number, then use switch statement
+        //else alert expression is incomplete
 }
+
+function addNumber(){
+    //when number button is pressed, number is concatenated to global string
+}
+
+function addOperator(){
+    //when operator button is pressed:
+    //if string is not empty and does not already contain an operator -- all operators use same function
+        //concat operator to string 
+}
+//global string for storing entire expression
+//add eventlistener to all btn [ignore decimal for now]
+
+        
