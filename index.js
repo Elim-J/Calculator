ece93a317b4842171bf51387a7c5a7493638bb18
elim-j <01CodePathways11@gmail.com>
2022-07-19 13:43:43 -0500

    Refactor code and comments

--- index.js
+++ index.js
@@ -1,86 +1,87 @@
-//add
 function add(a,b){
-    return parseInt(a) + parseInt(b);
+    render(parseInt(a) + parseInt(b));
 }
-//subtract
+
 function subtract(a,b){
-    return parseInt(a) - parseInt(b);
+    render(parseInt(a) - parseInt(b));
 }
-//multiply
+
 function multiply(a,b){
-    return parseInt(a) * parseInt(b);
+    render(parseInt(a) * parseInt(b));
 }
-//divide
+
 function divide(a, b){
     if(parseInt(b) === 0){
         alert("You can't divide by zero!");
     } else{
-        return parseInt(a) / parseInt(b);
+        render(parseInt(a) / parseInt(b));
     }
 }
-//evaluate
+function exponent(a, b){
+    render(parseInt(a) ** parseInt(b));
+}
+
 function evaluate(){
-    let regex = /(\d+)(\W)(\d+)/;    //separates expression into digit operator digit
-    let expressionArr = expression.match(regex); //first digit is at index 1
+    let regex = /(\d+)(\W)(\d+)/;    
+    let expressionArr = expression.match(regex);    //separate expression into [fullExpression, digits, operator, digits]
 
     if(expressionArr === null){
-        console.log("You need to enter an expression!");
+        alert("You need to enter an expression!");
     }else{
         switch(expressionArr[2]){
-        case "+": 
-            add(expressionArr[1], expressionArr[3]);
-            break;
-        case "-": 
-            subtract(expressionArr[1], expressionArr[3]);
-            break;
-        case "/": 
-            divide(expressionArr[1], expressionArr[3]);
-            break;
-        case "*": 
-            multiply(expressionArr[1], expressionArr[3]);
-            break;
-        default:
-            console.log("Something went terribly wrong");
+            case "+": 
+                add(expressionArr[1], expressionArr[3]);
+                break;
+            case "-": 
+                subtract(expressionArr[1], expressionArr[3]);
+                break;
+            case "/": 
+                divide(expressionArr[1], expressionArr[3]);
+                break;
+            case "*": 
+                multiply(expressionArr[1], expressionArr[3]);
+                break;
+            case "^": 
+                exponent(expressionArr[1], expressionArr[3]);
+                break;
+            default:
+                alert("Something went terribly wrong");
         }
     }
 }
 
-function addNumber(e){
-    //when number button is pressed, number is concatenated to global string
+//Append number to string expression
+function appendNumber(e){
     let number = e.target.id;
     expression += number;
-    console.log(expression);
+    render(expression);
 }
 
+//Append operator to string expression
 function appendOperator(e){
-    console.log(e.target.textContent); //this is the operator to concatenate to string
-    //when operator button is pressed:
-    let regex = /[^0-9]/;   //if there is no operator, it should be an empty string (deal with decimal later)
-                            //this regex takes all characters EXCEPT numbers
-    let tmp = expression.match(regex);
-    //if string is not empty and does not already contain an operator -- all operators use same function
-        //concat operator to string 
-    if(tmp === null && expression != ""){
+    let regex = /[^0-9]/;   //remove all numbers from expression                  
+    let operator = expression.match(regex);
+
+    if(operator === null && expression != ""){  //if no operator and expression has an integer
         expression += e.target.textContent;
-        console.log(expression);
-    }else {
-        alert("Cannot add operator");
+        render(expression)
+    } else {
+        alert("Cannot append operator");
     }
     
 }
 
-function render(){
-    //render string in the div
+//display expression and answer
+function render(answer){
+    calculatorScreen.textContent = answer;
 }
 
-//[ignore decimal btn for now]
-//global string for storing entire expression
 let expression = "";
 
 //add eventlistener to all number buttons 
 for(let i = 0; i<=9; i++){
     let numberBtn = document.getElementById(`${i}`);
-    numberBtn.addEventListener('click', addNumber);
+    numberBtn.addEventListener('click', appendNumber);
 }
 
 //get all operator btns
@@ -88,14 +89,18 @@ let divideBtn = document.getElementById('divide');
 let multiplyBtn = document.getElementById('multiply');
 let minusBtn = document.getElementById('minus');
 let plusBtn = document.getElementById('plus');
+let exponentBtn = document.getElementById('exponent');
 
 let equalBtn = document.getElementById('equal');
+let calculatorScreen = document.getElementById('number-display');
+
 
 //add eventlistener to all operator btn
 divideBtn.addEventListener('click', appendOperator);
 multiplyBtn.addEventListener('click', appendOperator);
 minusBtn.addEventListener('click', appendOperator);
 plusBtn.addEventListener('click', appendOperator);
+exponentBtn.addEventListener('click', appendOperator);
 
 equalBtn.addEventListener('click', evaluate);
 
