d78e88db2f41fe295073b17c20013801ec768e54
elim-j <01CodePathways11@gmail.com>
2022-07-19 12:24:13 -0500

    Update evaluate()

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 //add
 function add(a,b){
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 //subtract
 function subtract(a,b){
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 //multiply
 function multiply(a,b){
-    return a * b;
+    return parseInt(a) * parseInt(b);
 }
 //divide
 function divide(a, b){
-    if(b === 0){
+    if(parseInt(b) === 0){
         alert("You can't divide by zero!");
     } else{
-        return a / b;
+        return parseInt(a) / parseInt(b);
     }
 }
 //evaluate
-function evaluate(str){
-    //when equal sign is pressed
-    //if regex expression contains number symbol number, then use switch statement
-        //else alert expression is incomplete
+function evaluate(){
+    let regex = /(\d+)(\W)(\d+)/;    //separates expression into digit operator digit
+    let expressionArr = expression.match(regex); //first digit is at index 1
 
+    if(expressionArr === null){
+        console.log("You need to enter an expression!");
+    }else{
+        switch(expressionArr[2]){
+        case "+": 
+            add(expressionArr[1], expressionArr[3]);
+            break;
+        case "-": 
+            subtract(expressionArr[1], expressionArr[3]);
+            break;
+        case "/": 
+            divide(expressionArr[1], expressionArr[3]);
+            break;
+        case "*": 
+            multiply(expressionArr[1], expressionArr[3]);
+            break;
+        default:
+            console.log("Something went terribly wrong");
+        }
+    }
 }
 
 function addNumber(e){
@@ -33,7 +52,7 @@ function addNumber(e){
     console.log(expression);
 }
 
-function addOperator(e){
+function appendOperator(e){
     console.log(e.target.textContent); //this is the operator to concatenate to string
     //when operator button is pressed:
     let regex = /[^0-9]/;   //if there is no operator, it should be an empty string (deal with decimal later)
@@ -41,11 +60,11 @@ function addOperator(e){
     let tmp = expression.match(regex);
     //if string is not empty and does not already contain an operator -- all operators use same function
         //concat operator to string 
-    if(tmp === null){
+    if(tmp === null && expression != ""){
         expression += e.target.textContent;
         console.log(expression);
     }else {
-        alert("There is already an operator");
+        alert("Cannot add operator");
     }
     
 }
@@ -73,10 +92,10 @@ let plusBtn = document.getElementById('plus');
 let equalBtn = document.getElementById('equal');
 
 //add eventlistener to all operator btn
-divideBtn.addEventListener('click', addOperator);
-multiplyBtn.addEventListener('click', addOperator);
-minusBtn.addEventListener('click', addOperator);
-plusBtn.addEventListener('click', addOperator);
+divideBtn.addEventListener('click', appendOperator);
+multiplyBtn.addEventListener('click', appendOperator);
+minusBtn.addEventListener('click', appendOperator);
+plusBtn.addEventListener('click', appendOperator);
 
 equalBtn.addEventListener('click', evaluate);
 
